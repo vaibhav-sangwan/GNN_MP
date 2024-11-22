4b587300c3c3412fd8caf072a34748298f12a4d6
vaibhav-sangwan <sangwanvaibhav02@gmail.com>
2024-11-23 01:47:50 +0530

    Generate Embeddings

---
+++ generate_embbeding.py
@@ -0,0 +1,4 @@
+!python "/GNN/main.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m1.txt" --epoch 1 --dimensions 32
+!python "/GNN/main.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m2.txt" --epoch 1 --dimensions 32
+!python "/GNN/main.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m3.txt" --epoch 1 --dimensions 32
+!python "/GNN/main.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m4.txt" --epoch 1 --dimensions 32
